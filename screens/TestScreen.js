0c44fe3cfa997715f228b5c98cc8432f97fb39ff
m7md10 <m.alokaily@gmail.com>
2019-06-01 05:35:11 +0300

    Add files via upload

---
+++ screens/TestScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ExpoConfigView } from '@expo/samples';
+
+export default class TestScreen extends React.Component {
+  static navigationOptions = {
+    title: 'wtf',
+  };
+
+  render() {
+    return(
+        null
+    );
+  }
+}
